e5650f3152a0795d445ff57fcc6d41f9eaa82013
Tomás Pinto <tomascoelhopinto@gmail.com>
2017-11-18 10:45:10 +0000

    Push

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,17 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('5a100ec88b05d78916218b2d').then((todo) => {
+    console.log(todo);
+});

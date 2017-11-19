7edf2f4cf5c32594287f5ef3dc49e079afcf3afe
Tomás Pinto <tomascoelhopinto@gmail.com>
2017-11-19 14:00:56 +0000

    Message

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,30 @@
-// const {SHA256} = require('crypto-js');
+const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
 
-var token = jwt.sign(data, '123abc');
+var password = '123abc';
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$sQ0w41/OTzD5nbDq2gGP9eh6awYQZTslZcojz/UzVLse3Acskr2EK';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
